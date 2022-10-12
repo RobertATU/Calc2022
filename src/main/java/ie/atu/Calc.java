ab6182fefa930a8b3467a9469788f914b7383661
G00394277 <G00394277@atu.ie>
2022-10-12 13:28:51 +0100

    Added delete function

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,21 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
+        delete();
+    }
+
+    public static void delete() {
+        System.out.println("Enter first value:");
+        Scanner inputs = new Scanner(System.in);
+        int firstVal = inputs.nextInt();
+
+        System.out.println("Enter second value:");
+        int secondVal = inputs.nextInt();
+
+        int result = firstVal - secondVal;
+        System.out.println("The result of " + firstVal + " - " + secondVal + " is " + result);
 
     }
 }
