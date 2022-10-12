a5b31d26b520eb0a60dfe9a2e04dfb3b0ccf4222
Patrick Hession <G00394619@atu.ie>
2022-10-12 13:44:33 +0100

    Multiply

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,23 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        Multiply();
+    }
+    public static void Multiply(){
 
+
+        System.out.println("PLease enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int A = inputs.nextInt();
+
+        System.out.println("PLease enter your second number: ");
+        int B = inputs.nextInt();
+
+
+        int result = A * B;
+        System.out.println("The total is  " + result);
     }
 }
